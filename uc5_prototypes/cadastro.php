278e66f547e81e24e5252cfdd1db8d3bb07cd5ef
MaikyG <maikygabriel3@gmail.com>
2022-06-21 16:54:50 -0300

    css

--- uc5_prototypes/cadastro.php
+++ uc5_prototypes/cadastro.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro</title>
+  <link rel="stylesheet" href="css/cadastro.css">
 </head>
 <body>
-    <header></header>
-
+  
+</body>
+</html>
     <main>
     <div class="content">
       <div id="login">
@@ -49,7 +50,3 @@
       </div>
     </main>
 
-    <footer></footer>
-
-</body>
-</html>
