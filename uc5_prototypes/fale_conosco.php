3ac7810c1458460f3ba5667678dc75479bc3002e
MaikyG <maikygabriel3@gmail.com>
2022-06-22 16:55:49 -0300

    CSS da página Fale conosco

--- uc5_prototypes/fale_conosco.php
+++ uc5_prototypes/fale_conosco.php
@@ -5,98 +5,96 @@ require "cabecalho.php"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
-    <!-- <link rel="stylesheet" href="css/fale-conosco.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> 
+    <link rel="stylesheet" href="css/fale-conosco.css">
 <style>
 
 .content{
     width: 250px;
-    min-height: 120px;    
-    margin: 25px auto;
-    position: relative;   
-    display: inline;
-    flex-direction: column;
+       
+    margin: 20px ;
+    /* position: relative; */   
+    
   }
 
-  .correcao{
-    margin: 25px auto;
-    width: 400px;
-  }
+  
 
-
+.teste {
+    margin: 25px;
+    
+}
 
 
 </style> -->
 
 </head>
-<main>
-        <article class="conteudo limitador">
+<main class="w-50 mx-auto">
+        <article class="w-75 mx-auto">
 
             <h2>Contato</h2>
     
-            <p>Fale conosco mandando um menssagem neste formulario.</p>
+            <p class="d-flex flex-nowrap">Fale conosco mandando um menssagem neste formulario.</p>
 
             
-            <form id="my-form" action="" method="post">
+            <form  id="my-form" action="" method="post">
 <table class="w3-table">
 <tr>
                 <div>
-                   <td> <label for="nome">Nome:</label> </td>
-                   <td> <input placeholder="nome" type="text" name="" id="nome" required> </td>
+                   <td class="text-primary"> <label for="nome">Nome:</label> </td>
+                   <td > <input class="border border-primary" placeholder="nome" type="text" name="" id="nome" required> </td>
                 </div>
                 </tr>
 
                 <tr>
                 <div class="content">
-                <td>  <label for="email">E-mail:</label> </td>
-                <td>  <input placeholder="email" type="email" name="" id="email" required> </td>
+                <td class="text-primary">  <label for="email">E-mail:</label> </td>
+                <td>  <input class="border border-primary" placeholder="email" type="email" name="" id="email" required> </td>
                 </div>
                 </tr>
 
                 <tr>
                 <div class="content">
-                <td>   <label for="data">Data de nascimento:</label> </td>
-                <td>   <input type="date" name="data" id="data" required> </td>
+                <td class="text-primary">   <label for="data">Data de nascimento:</label> </td>
+                <td>   <input class="border border-primary" type="date" name="data" id="data" required> </td>
                 </div>
                 </tr>
 
                 <tr>
                 <div class="content">
-                <td>   <label for="telefone">Telefone:</label> </td>
-                <td>   <input placeholder="telefone" type="tel" name="telefone" id="telefone"> </td>
+                <td class="text-primary">   <label for="telefone">Telefone:</label> </td>
+                <td>   <input class="border border-primary" placeholder="telefone" type="tel" name="telefone" id="telefone"> </td>
                 </div>
                 </tr>
 
                 <tr>
                 <div class="content">
-                <td>   <label for="idade">Idade:</label> </td>
-                <td>  <input placeholder="idade" type="number" name="idade" id="idade" min="18" max="100" required> </td>
+                <td class="text-primary">   <label for="idade">Idade:</label> </td>
+                <td>  <input class="border border-primary" placeholder="idade" type="number" name="idade" id="idade" min="18" max="100" required> </td>
                 </div>
                 </tr>
                 </table>
 
-                <div class="sexo">
-                   <p>Sexo:</p> </td>
-                   <input value="Masculino" type="radio" name="sexo" id="masculimo"> 
-                   <label for="masculimo">Masculimo</label> 
+                <div class="teste">
+                   <p >Sexo:</p> 
+                   <input class=" mx-2" value="Masculino" type="radio" name="sexo" id="masculimo"> 
+                   <label class="w-25 border border-primary" for="masculimo">Masculimo</label> 
 
                    <input value="Feminino" type="radio" name="sexo" id="feminino"> 
-                   <label for="feminino">Feminino</label> 
+                   <label class="border border-primary" for="feminino">Feminino</label> 
                 </div>
                 
 
 
-                <div class="content">
-                   <p>Interesses</p> </td>
-                   <input value="Desing" type="checkbox" name="interesses" id="desing"> 
-                   <label for="desing">Desing</label> 
+                <div class="teste">
+                   <p >Interesses</p> </td>
+                   <input class=" mx-1" value="Desing" type="checkbox" name="interesses" id="desing"> 
+                   <label class="w-25 borda" for="desing">Desing</label> 
 
-                   <input value="Robotica" type="checkbox" name="interesses" id="robotica"> 
-                   <label for="robotica">Robotica</label> 
+                   <input class=" mx-1" value="Robotica" type="checkbox" name="interesses" id="robotica"> 
+                   <label class="w-25 borda" for="robotica">Robotica</label> 
 
-                   <input value="Programação" type="checkbox" name="interesses" id="programacao"> 
-                   <label for="programacao">Programação</label> 
+                   <input class=" mx-1" value="Programação" type="checkbox" name="interesses" id="programacao"> 
+                   <label class="w-25 borda" for="programacao">Programação</label> 
                 </div>
                 
 
@@ -105,7 +103,7 @@ require "cabecalho.php"
                 <tr>
                 <div class="content">
                 <td>   <label for="cep">CEP:</label> </td>
-                <td>   <input type="text" id="cep" name="cep" maxlength="9" required> </td>
+                <td class="borda">   <input type="text" id="cep" name="cep" maxlength="9" required> </td>
 </table>
                    <button id="localizar-cep">Localizar</button> 
                     <b id="status"></b>
@@ -114,30 +112,30 @@ require "cabecalho.php"
                 </tr>
 
                 <tr>
-                <div class="content correcao">
+                <div class="content ">
                 <td>   <label for="endereco">Endereço:</label> </td>
-                <td>   <input type="text" id="endereco" name="endereco" size="30"> </td>
+                <td class="borda">   <input type="text" id="endereco" name="endereco" size="30"> </td>
                 </div>
                 </tr>
 
                 <tr>
                 <div class="content">
                 <td>   <label for="bairro">Bairro:</label> </td>
-                <td>   <input type="text" id="bairro" name="bairro"> </td>
+                <td class="borda">   <input type="text" id="bairro" name="bairro"> </td>
                 </div>
                 </tr>
 
                 <tr>
                 <div class="content">
                 <td>   <label for="cidade">Cidade:</label> </td>
-                <td>   <input type="text" id="cidade" name="cidade"> </td>
+                <td class="borda">   <input type="text" id="cidade" name="cidade"> </td>
                 </div>
                 </tr>
 
                 <tr>
                 <div class="content">
                 <td>   <label for="estado">Estado:</label> </td>
-                <td>   <input type="text" id="estado" name="estado"> </td>
+                <td class="borda">   <input type="text" id="estado" name="estado"> </td>
                 </div>
                 </tr>
                 </table>
@@ -146,7 +144,7 @@ require "cabecalho.php"
                 
                 <div class="content">
                    <label for="assunto">Assunto:</label> 
-                    <select name="" id=""> 
+                    <select class="borda" name="" id=""> 
                         <option></option> 
                        <option>Duvida</option> 
                        <option>Elogio</option> 
